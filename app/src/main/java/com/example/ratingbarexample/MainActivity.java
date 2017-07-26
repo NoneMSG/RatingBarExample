0d9cea96dc93db6781f9f6120914d25407eefdc0
bit-user <kickscar@gmail.com>
2017-07-26 16:34:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ratingbarexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ratingbarexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((RatingBar)findViewById( R.id.ratingBar1 )).
+                setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        ((TextView)findViewById( R.id.editText )).
+                                setText( "rating:" + rating );
+                    }
+                });
+    }
+}
